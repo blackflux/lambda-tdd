33f3c6cd9b1c35571e4ec1d229332d7dd682e963
blackflux <blackflux@unfine.de>
2017-11-18 20:05:14 -0800

    feat: will now pick up test files automatically

--- test/test_tester.js
+++ test/test_tester.js
@@ -1,12 +1,8 @@
 const path = require("path");
-const globSync = require("glob").sync;
 const lambdaTester = require("./../lib/tester")({
   cwd: path.join(__dirname, "sample")
 });
 
 describe("Testing Tester", () => {
-  lambdaTester.execute(globSync("**/test_*.json", {
-    cwd: path.join(__dirname, "sample"),
-    nodir: true
-  }).filter(e => !e.startsWith("__cassettes/")));
+  lambdaTester.execute();
 });
