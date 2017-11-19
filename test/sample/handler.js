7c4173c7c6ba05a51a277a583ba0c9a8ddb4bcb7
blackflux <blackflux@unfine.de>
2017-11-18 17:09:45 -0800

    chore: added test for freezing timestamp chore: minimized configuration files for test

--- test/sample/handler.js
+++ test/sample/handler.js
@@ -1,2 +1,3 @@
 module.exports.returnEvent = (event, context, callback) => callback(null, event);
 module.exports.returnEnv = (event, context, callback) => callback(null, process.env);
+module.exports.returnUnix = (event, context, callback) => callback(null, { unix: Math.floor(new Date() / 1000) });
