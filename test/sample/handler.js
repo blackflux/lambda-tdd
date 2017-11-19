ab38376889e5c6d1ba236c234375adf482286335
blackflux <blackflux@unfine.de>
2017-11-18 18:18:57 -0800

    chore: test for error response

--- test/sample/handler.js
+++ test/sample/handler.js
@@ -1,6 +1,7 @@
 const request = require("request");
 
 module.exports.returnEvent = (event, context, cb) => cb(null, event);
+module.exports.returnError = (event, context, cb) => cb("error");
 module.exports.returnEnv = (event, context, cb) => cb(null, process.env);
 module.exports.returnUnix = (event, context, cb) => cb(null, { unix: Math.floor(new Date() / 1000) });
 module.exports.returnTimeout = (event, context, cb) => cb(null, { timeout: context.getRemainingTimeInMillis() });
