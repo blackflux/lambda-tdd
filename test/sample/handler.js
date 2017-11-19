09fc70b333a7241b6814eb2c8ccb9775d31d84b6
blackflux <blackflux@unfine.de>
2017-11-18 17:01:34 -0800

    fix: changed where test env variables are unset chore: added test for env variables

--- test/sample/handler.js
+++ test/sample/handler.js
@@ -1,1 +1,2 @@
-module.exports.handler = (event, context, callback) => callback(null, event);
+module.exports.returnEvent = (event, context, callback) => callback(null, event);
+module.exports.returnEnv = (event, context, callback) => callback(null, process.env);
