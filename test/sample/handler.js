751a41c6d1d5f7e29e9cf3e565a6c614490df17f
blackflux <blackflux@unfine.de>
2017-11-18 17:20:08 -0800

    chore: added lambda timeout tests

--- test/sample/handler.js
+++ test/sample/handler.js
@@ -1,3 +1,4 @@
-module.exports.returnEvent = (event, context, callback) => callback(null, event);
-module.exports.returnEnv = (event, context, callback) => callback(null, process.env);
-module.exports.returnUnix = (event, context, callback) => callback(null, { unix: Math.floor(new Date() / 1000) });
+module.exports.returnEvent = (event, context, cb) => cb(null, event);
+module.exports.returnEnv = (event, context, cb) => cb(null, process.env);
+module.exports.returnUnix = (event, context, cb) => cb(null, { unix: Math.floor(new Date() / 1000) });
+module.exports.returnTimeout = (event, context, cb) => cb(null, { timeout: context.getRemainingTimeInMillis() });
