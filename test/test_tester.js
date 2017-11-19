3fa6e0e7af739fa9a7aa5d72b69eab0efcc9cf0b
blackflux <blackflux@unfine.de>
2017-11-18 19:49:54 -0800

    feat: reasonable defaults are now set

--- test/test_tester.js
+++ test/test_tester.js
@@ -1,17 +1,12 @@
 const path = require("path");
 const globSync = require("glob").sync;
 const lambdaTester = require("./../lib/tester")({
-  name: "lambda-test",
-  envVarYml: path.join(__dirname, "sample", "env.yml"),
-  debug: false,
-  handlerFile: path.join(__dirname, "sample", "handler.js"),
-  cassetteFolder: path.join(__dirname, "sample", "__cassettes"),
-  testFolder: path.join(__dirname, "sample")
+  cwd: path.join(__dirname, "sample")
 });
 
 describe("Testing Tester", () => {
   lambdaTester.execute(globSync("**/test_*.json", {
-    nodir: true,
-    cwd: path.join(__dirname, "sample")
+    cwd: path.join(__dirname, "sample"),
+    nodir: true
   }).filter(e => !e.startsWith("__cassettes/")));
 });
