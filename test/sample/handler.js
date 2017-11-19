3a215f0999f79f9785d07f15a4266f8b7334814b
blackflux <blackflux@unfine.de>
2017-11-18 18:14:48 -0800

    chore: added external request nock test

--- test/sample/handler.js
+++ test/sample/handler.js
@@ -1,4 +1,8 @@
+const request = require("request");
+
 module.exports.returnEvent = (event, context, cb) => cb(null, event);
 module.exports.returnEnv = (event, context, cb) => cb(null, process.env);
 module.exports.returnUnix = (event, context, cb) => cb(null, { unix: Math.floor(new Date() / 1000) });
 module.exports.returnTimeout = (event, context, cb) => cb(null, { timeout: context.getRemainingTimeInMillis() });
+module.exports.returnExternal = (event, context, cb) => request
+  .get("http://ip-api.com/json", { json: true }, (err, res, body) => cb(err, body));
