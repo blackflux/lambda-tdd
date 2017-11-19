7971a2ebfa5495058572b13172e85c1d914befd0
blackflux <blackflux@unfine.de>
2017-11-18 19:27:19 -0800

    - generified silence and debug -> verbose - added microservice keyword

--- test/modules/test_consoleRecorder.js
+++ test/modules/test_consoleRecorder.js
@@ -2,31 +2,31 @@
 const ConsoleRecorder = require('../../lib/modules/consoleRecorder');
 const expect = require("chai").expect;
 
-const testConsole = (silence) => {
+const testConsole = (verbose) => {
   const logs = [];
   const consoleLogOriginal = console.log;
   console.log = (...args) => {
     logs.push(...args);
   };
-  const consoleRecorder = ConsoleRecorder({ silence });
+  const consoleRecorder = ConsoleRecorder({ verbose });
   consoleRecorder.start();
   console.log("test-log1");
   console.log("test-log2");
   expect(consoleRecorder.finish()).to.deep.equal(["test-log1", "test-log2"]);
-  if (silence === true) {
-    expect(logs).to.deep.equal([]);
-  } else {
+  if (verbose === true) {
     expect(logs).to.deep.equal(["test-log1", "test-log2"]);
+  } else {
+    expect(logs).to.deep.equal([]);
   }
   console.log = consoleLogOriginal;
 };
 
 describe("Testing ConsoleRecorder", () => {
   it("Testing Logging Silent", () => {
-    testConsole(true);
+    testConsole(false);
   });
 
   it("Testing Logging Verbose", () => {
-    testConsole(false);
+    testConsole(true);
   });
 });
