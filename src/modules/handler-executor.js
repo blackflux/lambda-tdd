e5377340c41748b13bee463456c5dad6eb1fddfe
Lukas Siemon <github@blackflux.com>
2019-07-22 15:50:04 -0700

    feat: testing cassette recording order

--- src/modules/handler-executor.js
+++ src/modules/handler-executor.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const wrapper = require('lambda-wrapper');
 const nock = require('nock');
 const ConsoleRecorder = require('./console-recorder');
@@ -12,8 +14,13 @@ module.exports = (options) => {
 
   return {
     execute: () => new Promise((resolve) => {
+      const cassetteFile = path.join(options.cassetteFolder, options.cassetteFile);
+      const unmatched = fs.existsSync(cassetteFile) ? nock.load(cassetteFile) : null;
+      const checkOrder = unmatched !== null && unmatched.length > 1;
+      const outOfOrderErrors = [];
+
       consoleRecorder.start();
-      nockBack.setMode('record');
+      nockBack.setMode(unmatched === null ? 'record' : 'lockdown');
       nockBack.fixtures = options.cassetteFolder;
       nockBack(options.cassetteFile, {
         before: (r) => {
@@ -22,6 +29,28 @@ module.exports = (options) => {
             Object.assign(r, { body: '*' });
           }
           return r;
+        },
+        after: (scope) => {
+          scope.on('request', (req, interceptor) => {
+            if (checkOrder) {
+              // eslint-disable-next-line no-underscore-dangle
+              const matchedKey = interceptor.scope.interceptors[0]._key;
+
+              let count = 0;
+              const check = () => {
+                // eslint-disable-next-line no-underscore-dangle
+                if (matchedKey === unmatched[0].interceptors[0]._key) {
+                  unmatched.splice(0, 1);
+                } else if (count < 100) {
+                  count += 1;
+                  process.nextTick(check);
+                } else {
+                  outOfOrderErrors.push(matchedKey);
+                }
+              };
+              check();
+            }
+          });
         },
         afterRecord: recordings => (options.stripHeaders === true ? recordings.map((r) => {
           const res = Object.assign({}, r);
@@ -45,7 +74,8 @@ module.exports = (options) => {
             err,
             response,
             logs: consoleRecorder.finish(),
-            pendingMocks
+            pendingMocks,
+            outOfOrderErrors
           });
         });
       });
