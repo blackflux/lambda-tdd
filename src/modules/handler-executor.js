c59ae386da13285f744d05ca188cf8dbc682b6ef
Lukas Siemon <github@blackflux.com>
2020-07-17 10:46:20 -0700

    amend: minor

--- src/modules/handler-executor.js
+++ src/modules/handler-executor.js
@@ -8,19 +8,18 @@ module.exports = (options) => {
 
   return {
     execute: async () => {
-      const modifiers = options.modifiers;
       const requestRecorder = RequestRecorder({
         cassetteFolder: options.cassetteFolder,
         stripHeaders: options.stripHeaders || false,
         strict: false,
         heal: options.nockHeal,
-        modifiers
+        modifiers: options.modifiers
       });
       await requestRecorder.inject(options.cassetteFile);
 
       const startTimestamp = process.hrtime();
       const startTime = (startTimestamp[0] * 1000) + (startTimestamp[1] / 1000000);
-      const event = rewriteObject(options.event, modifiers);
+      const event = rewriteObject(options.event, options.modifiers);
       const [err, response] = await new Promise((resolve) => runner.run(event, {
         ...options.context,
         getRemainingTimeInMillis: () => {
